38e17c31165296aca1ee52188744f5f5f2b6d75e
shivapro <75524510+shivapro@users.noreply.github.com>
2021-04-28 15:58:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas, backgroundimage,database;
+var position;
+var gameState=0;
+var playerCount=0;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+
+  game=new Game();
+
+  game.getState();
+
+  game.start();
+
+
+
+
+}
+
+function draw(){
+    
+}
+
